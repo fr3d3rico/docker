1bdc5ccbb4d649bbefcf4cd7a452482aec968754
Fred <frederico.a.soares@gmail.com>
2019-06-12 18:31:38 -0300

    Added mongoose test.

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+const Cat = mongoose.model('Cat', { name: String });
 
 const PORT = 8080;
 
@@ -6,6 +10,23 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello Docker!');
+});
+
+app.get('/save', (req, res) => {
+    
+    const kitty = new Cat({ name: 'Zildjian' });
+    kitty.save().then(() => console.log('meow'));
+
+    res.send('Cat saved!');
+});
+
+app.get('/list', (req, res) => {
+
+    Cat.find(function (err, kittens) {
+        if (err) return res.send(err);
+        res.send(kittens);
+    })
+    
 });
 
 app.listen(PORT, () => {
