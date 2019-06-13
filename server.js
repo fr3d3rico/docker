261f7e3a977986eb1c5dd3de1814855f91f05c3d
Fred <frederico.a.soares@gmail.com>
2019-06-13 15:47:43 -0300

    Update server.js
    Mongoose on and once events.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+var db = mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 const Cat = mongoose.model('Cat', { name: String });
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  console.log("h");
+});
 
 const PORT = 8080;
 
