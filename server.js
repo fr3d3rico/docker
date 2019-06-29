d17b486232b077942170c2cd6309f6d5572a4cfa
Fred <frederico.a.soares@gmail.com>
2019-06-29 11:57:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://172.17.0.2:27017/test', {useNewUrlParser: true});
 
 var db = mongoose.connection;
 
